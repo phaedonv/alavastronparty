c797995589c1077f04b16b2e78a75354b0c1eeb8
phaedonv <faival@protonmail.com>
2021-07-06 10:44:17 +0200

    init

---
+++ main.js
@@ -0,0 +1,15 @@
+function createPoster() {
+    for(let i = 0; i < 100; i++) {
+        const img = document.createElement("img");
+        img.src = "https://picsum.photos/200/301?id=" + i;
+        if(i === 5) {
+          img.style.borderRadius = "50%";
+        }
+        img.classList.add("img-margin");
+        document.body.appendChild(img);
+      }
+}
+
+createPoster();
+
+
