5ca8d4455bbc1fbdb348fcea0f20bdf883e0b673
phaedonv <faival@protonmail.com>
2021-07-07 12:23:18 +0200

    added all the links and fixed some ids

--- main.js
+++ main.js
@@ -129,346 +129,349 @@ posterData = [{
     "picture" : "v1625592059/Alavastron%20posters/Soundtrack_vol_III_pd9n6q.jpg"
 }, {
     "id" : 44,
-    "picture" : "v1621354287/logo1_n4on1d.png"
+    "picture" : "v1625592057/Alavastron%20posters/santa_xionia_disco_aswftq.jpg"
 }, {
     "id" : 45,
-    "picture" : "v1621354287/logo1_n4on1d.png"
+    "picture" : "v1625592057/Alavastron%20posters/santa_red_party_h2tafi.jpg"
 }, {
     "id" : 46,
-    "picture" : "v1621354287/logo1_n4on1d.png"
+    "picture" : "v1625592056/Alavastron%20posters/saint_party_nq2m1j.jpg"
 }, {
     "id" : 47,
-    "picture" : "v1621354287/logo1_n4on1d.png"
+    "picture" : "v1625592055/Alavastron%20posters/POSTER._qknojb.jpg"
 }, {
     "id" : 48,
-    "picture" : "v1621354287/logo1_n4on1d.png"
+    "picture" : "v1625592055/Alavastron%20posters/rock_roll_eve_xhtqmb.jpg"
 }, {
     "id" : 49,
-    "picture" : "v1621354287/logo1_n4on1d.png"
+    "picture" : "v1625592055/Alavastron%20posters/rock_roll_16_1_2015_q7nzge.jpg"
 }, {
     "id" : 50,
-    "picture" : "v1621354287/logo1_n4on1d.png"
+    "picture" : "v1625592054/Alavastron%20posters/postt_ea2dss.jpg"
 }, {
     "id" : 51,
-    "picture" : "v1621354287/logo1_n4on1d.png"
+    "picture" : "v1625592053/Alavastron%20posters/RETROW4VE_bv1noy.png"
 }, {
     "id" : 52,
-    "picture" : "v1621354287/logo1_n4on1d.png"
+    "picture" : "v1625592052/Alavastron%20posters/rembetikoN_ruarok.jpg"
 }, {
     "id" : 53,
-    "picture" : "v1621354287/logo1_n4on1d.png"
+    "picture" : "v1625592051/Alavastron%20posters/rock_roll_ulxtxy.jpg"
 }, {
     "id" : 54,
-    "picture" : "v1621354287/logo1_n4on1d.png"
+    "picture" : "v1625592049/Alavastron%20posters/poster-paularas_pvjd3j.jpg"
 }, {
     "id" : 55,
-    "picture" : "v1621354287/logo1_n4on1d.png"
+    "picture" : "v1625592046/Alavastron%20posters/paganini90s_kcx9tg.jpg"
 }, {
     "id" : 56,
-    "picture" : "v1621354287/logo1_n4on1d.png"
+    "picture" : "v1625592046/Alavastron%20posters/poster_final_ken3ya.png"
 }, {
     "id" : 57,
-    "picture" : "v1621354287/logo1_n4on1d.png"
+    "picture" : "v1625592045/Alavastron%20posters/POSTERAKI_icmjxk.jpg"
 }, {
     "id" : 58,
-    "picture" : "v1621354287/logo1_n4on1d.png"
+    "picture" : "v1625592044/Alavastron%20posters/posterONE_soghxp.jpg"
 }, {
     "id" : 59,
-    "picture" : "v1621354287/logo1_n4on1d.png"
+    "picture" : "v1625592041/Alavastron%20posters/new_years_eve_2015_hmo57u.bmp"
 }, {
     "id" : 60,
-    "picture" : "v1621354287/logo1_n4on1d.png"
+    "picture" : "v1625592039/Alavastron%20posters/POSTER_ACTIVE_MEMBER_small_l1k5bp.jpg"
 }, {
     "id" : 61,
-    "picture" : "v1621354287/logo1_n4on1d.png"
+    "picture" : "v1625592036/Alavastron%20posters/pop_rock_auu2dc.jpg"
 }, {
     "id" : 62,
-    "picture" : "v1621354287/logo1_n4on1d.png"
+    "picture" : "v1625592036/Alavastron%20posters/poster_daofol.jpg"
 }, {
     "id" : 63,
-    "picture" : "v1621354287/logo1_n4on1d.png"
+    "picture" : "v1625592032/Alavastron%20posters/nype_hitman_a3_povwkc.png"
 }, {
     "id" : 64,
-    "picture" : "v1621354287/logo1_n4on1d.png"
+    "picture" : "v1625592032/Alavastron%20posters/PicsArt_11-24-07.59.44.gif_cso9bp.jpg"
 }, {
     "id" : 65,
-    "picture" : "v1621354287/logo1_n4on1d.png"
+    "picture" : "v1625592032/Alavastron%20posters/ONE_party_bdu0lp.png"
 }, {
     "id" : 66,
-    "picture" : "v1621354287/logo1_n4on1d.png"
+    "picture" : "v1625592029/Alavastron%20posters/lastcarnival_s88wk1.jpg"
 }, {
     "id" : 67,
-    "picture" : "v1621354287/logo1_n4on1d.png"
+    "picture" : "v1625592028/Alavastron%20posters/party_xlqczl.jpg"
 }, {
     "id" : 68,
-    "picture" : "v1621354287/logo1_n4on1d.png"
+    "picture" : "v1625592027/Alavastron%20posters/newYearsEve2018_ybl70q.png"
 }, {
     "id" : 69,
-    "picture" : "v1621354287/logo1_n4on1d.png"
+    "picture" : "v1625592026/Alavastron%20posters/music_is_freedom_bowie_2018_m4oog6.png"
 }, {
     "id" : 70,
-    "picture" : "v1621354287/logo1_n4on1d.png"
+    "picture" : "v1625592024/Alavastron%20posters/new_retro_wave_th6cca.jpg"
 }, {
     "id" : 71,
-    "picture" : "v1621354287/logo1_n4on1d.png"
+    "picture" : "v1625592024/Alavastron%20posters/music_is_my_aeroplane_g1rpgg.jpg"
 }, {
     "id" : 72,
-    "picture" : "v1621354287/logo1_n4on1d.png"
+    "picture" : "v1625592021/Alavastron%20posters/helloworld_e52ilv.png"
 }, {
     "id" : 73,
-    "picture" : "v1621354287/logo1_n4on1d.png"
+    "picture" : "v1625592022/Alavastron%20posters/music_mashup_party2_yjfl7x.jpg"
 }, {
     "id" : 74,
-    "picture" : "v1621354287/logo1_n4on1d.png"
+    "picture" : "v1625592021/Alavastron%20posters/mayTheVosBWithU_cswqz2.jpg"
 }, {
     "id" : 75,
-    "picture" : "v1621354287/logo1_n4on1d.png"
+    "picture" : "v1625592020/Alavastron%20posters/mixtape_party_rnlkmr.jpg"
 }, {
     "id" : 76,
-    "picture" : "v1621354287/logo1_n4on1d.png"
+    "picture" : "v1625592021/Alavastron%20posters/music_mashup_party_ajizem.jpg"
 }, {
     "id" : 77,
-    "picture" : "v1621354287/logo1_n4on1d.png"
+    "picture" : "v1625592018/Alavastron%20posters/masqueradeParty_pg3wfb.png"
 }, {
     "id" : 78,
-    "picture" : "v1621354287/logo1_n4on1d.png"
+    "picture" : "v1625592017/Alavastron%20posters/manolakis_tattoo_party_um6zxy.jpg"
 }, {
     "id" : 79,
-    "picture" : "v1621354287/logo1_n4on1d.png"
+    "picture" : "v1625592016/Alavastron%20posters/m1naz_vu5wob.png"
 }, {
     "id" : 80,
-    "picture" : "v1621354287/logo1_n4on1d.png"
+    "picture" : "v1625592016/Alavastron%20posters/Lose_yourself_2_90s_version_wzo0fg.png"
 }, {
     "id" : 81,
-    "picture" : "v1621354287/logo1_n4on1d.png"
+    "picture" : "v1625592016/Alavastron%20posters/mad_as_hell_obbpwc.jpg"
 }, {
     "id" : 82,
-    "picture" : "v1621354287/logo1_n4on1d.png"
+    "picture" : "v1625592015/Alavastron%20posters/lose_yourselfA3_eqik8q.jpg"
 }, {
     "id" : 83,
-    "picture" : "v1621354287/logo1_n4on1d.png"
+    "picture" : "v1625592013/Alavastron%20posters/keep_on_surfing_pARTY_m9meog.png"
 }, {
     "id" : 84,
-    "picture" : "v1621354287/logo1_n4on1d.png"
+    "picture" : "v1625592012/Alavastron%20posters/LETITHAPPEN_zpic9j.png"
 }, {
     "id" : 85,
-    "picture" : "v1621354287/logo1_n4on1d.png"
+    "picture" : "v1625592011/Alavastron%20posters/JOKER_eu5mmc.jpg"
 }, {
     "id" : 86,
-    "picture" : "v1621354287/logo1_n4on1d.png"
+    "picture" : "v1625592011/Alavastron%20posters/listen_sk0k6q.jpg"
 }, {
     "id" : 87,
-    "picture" : "v1621354287/logo1_n4on1d.png"
+    "picture" : "v1625592009/Alavastron%20posters/jiniHENDRIX_tmefqv.png"
 }, {
     "id" : 88,
-    "picture" : "v1621354287/logo1_n4on1d.png"
+    "picture" : "v1625592008/Alavastron%20posters/iptamenodisco_uoi7ci.jpg"
 }, {
     "id" : 89,
-    "picture" : "v1621354287/logo1_n4on1d.png"
+    "picture" : "v1625592007/Alavastron%20posters/final_rock_roll_party_xj5jrn.png"
 }, {
     "id" : 90,
-    "picture" : "v1621354287/logo1_n4on1d.png"
+    "picture" : "v1625592007/Alavastron%20posters/HAPPYNEW2016_jlvyw6.jpg"
 }, {
     "id" : 91,
-    "picture" : "v1621354287/logo1_n4on1d.png"
+    "picture" : "v1625592005/Alavastron%20posters/discopartyBLUE_aw8b6c.png"
 }, {
     "id" : 92,
-    "picture" : "v1621354287/logo1_n4on1d.png"
+    "picture" : "v1625592004/Alavastron%20posters/happy_birthday_4_thgqpr.jpg"
 }, {
     "id" : 93,
-    "picture" : "v1621354287/logo1_n4on1d.png"
+    "picture" : "v1625592003/Alavastron%20posters/goulioA3_yu58by.png"
 }, {
     "id" : 94,
-    "picture" : "v1621354287/logo1_n4on1d.png"
+    "picture" : "v1625592004/Alavastron%20posters/greek_partry_2015_fahvu9.jpg"
 }, {
     "id" : 95,
-    "picture" : "v1621354287/logo1_n4on1d.png"
+    "picture" : "v1625592003/Alavastron%20posters/funk_off_huvxbh.jpg"
 }, {
     "id" : 96,
-    "picture" : "v1621354287/logo1_n4on1d.png"
+    "picture" : "v1625592001/Alavastron%20posters/greekparty2015_rll2sk.jpg"
 }, {
     "id" : 97,
-    "picture" : "v1621354287/logo1_n4on1d.png"
+    "picture" : "v1625591998/Alavastron%20posters/FROMDUSKTILLDAWN_c9g7yt.png"
 }, {
     "id" : 98,
-    "picture" : "v1621354287/logo1_n4on1d.png"
+    "picture" : "v1625591996/Alavastron%20posters/gettogetherfeelallright_spbrye.png"
 }, {
     "id" : 99,
-    "picture" : "v1621354287/logo1_n4on1d.png"
+    "picture" : "v1625591994/Alavastron%20posters/fridaythe13th_jvi236.jpg"
 }, {
     "id" : 100,
-    "picture" : "v1621354287/logo1_n4on1d.png"
+    "picture" : "v1625591993/Alavastron%20posters/final_small_lxi7b4.png"
 }, {
     "id" : 101,
-    "picture" : "v1621354287/logo1_n4on1d.png"
+    "picture" : "v1625591991/Alavastron%20posters/final_khzltj.jpg"
 }, {
     "id" : 102,
-    "picture" : "v1621354287/logo1_n4on1d.png"
+    "picture" : "v1625591990/Alavastron%20posters/freeyourself_axsjx0.png"
 }, {
     "id" : 103,
-    "picture" : "v1621354287/logo1_n4on1d.png"
+    "picture" : "v1625591990/Alavastron%20posters/fight4yourighttoparty_rm68rm.jpg"
 }, {
     "id" : 104,
-    "picture" : "v1621354287/logo1_n4on1d.png"
+    "picture" : "v1625591989/Alavastron%20posters/fight4yourright_cr39ld.jpg"
 }, {
     "id" : 105,
-    "picture" : "v1621354287/logo1_n4on1d.png"
+    "picture" : "v1625591987/Alavastron%20posters/dance_2_be_alive_o1moeq.png"
 }, {
     "id" : 106,
-    "picture" : "v1621354287/logo1_n4on1d.png"
+    "picture" : "v1625591987/Alavastron%20posters/final_2_bvxw8n.jpg"
 }, {
     "id" : 107,
-    "picture" : "v1621354287/logo1_n4on1d.png"
+    "picture" : "v1625591985/Alavastron%20posters/dONTPanicPartyVol2_ruzk5d.png"
 }, {
     "id" : 108,
-    "picture" : "v1621354287/logo1_n4on1d.png"
+    "picture" : "v1625591985/Alavastron%20posters/exam_is_over_thou_shalt_do_the_dance_z7u31m.jpg"
 }, {
     "id" : 109,
-    "picture" : "v1621354287/logo1_n4on1d.png"
+    "picture" : "v1625591985/Alavastron%20posters/dONTPanicParty_j4ycdn.png"
 }, {
     "id" : 110,
-    "picture" : "v1621354287/logo1_n4on1d.png"
+    "picture" : "v1625591983/Alavastron%20posters/EVEning_sunday_adxzbq.png"
 }, {
     "id" : 111,
-    "picture" : "v1621354287/logo1_n4on1d.png"
+    "picture" : "v1625591982/Alavastron%20posters/epic_disco_party_2017_xt9rhw.jpg"
 }, {
     "id" : 112,
-    "picture" : "v1621354287/logo1_n4on1d.png"
+    "picture" : "v1625591980/Alavastron%20posters/DISobeyPARTY_goqanm.jpg"
 }, {
     "id" : 113,
-    "picture" : "v1621354287/logo1_n4on1d.png"
+    "picture" : "v1625591979/Alavastron%20posters/disco_penny_friday_hr2ejj.jpg"
 }, {
     "id" : 114,
-    "picture" : "v1621354287/logo1_n4on1d.png"
+    "picture" : "v1625591978/Alavastron%20posters/alavastronSurfingFriday2_vk1w0n.png"
 }, {
     "id" : 115,
-    "picture" : "v1621354287/logo1_n4on1d.png"
+    "picture" : "v1625591978/Alavastron%20posters/cocktail_party_kinjlq.jpg"
 }, {
     "id" : 116,
-    "picture" : "v1621354287/logo1_n4on1d.png"
+    "picture" : "v1625591978/Alavastron%20posters/cocktail_g2gob5.png"
 }, {
     "id" : 117,
-    "picture" : "v1621354287/logo1_n4on1d.png"
+    "picture" : "v1625591976/Alavastron%20posters/discoparty_2014_e5btpk.jpg"
 }, {
     "id" : 118,
-    "picture" : "v1621354287/logo1_n4on1d.png"
+    "picture" : "v1625591975/Alavastron%20posters/BreathBaby_ggs9ep.png"
 }, {
     "id" : 119,
-    "picture" : "v1621354287/logo1_n4on1d.png"
+    "picture" : "v1625591975/Alavastron%20posters/bobross_y1n9qk.png"
 }, {
     "id" : 120,
-    "picture" : "v1621354287/logo1_n4on1d.png"
+    "picture" : "v1625591975/Alavastron%20posters/COCKTAIL_PARTY_2015_VOL_ii_aiword.jpg"
 }, {
     "id" : 121,
-    "picture" : "v1621354287/logo1_n4on1d.png"
+    "picture" : "v1625591973/Alavastron%20posters/bobrossSantA_pgfbcn.png"
+}, {
+    "id" : 122,
+    "picture" : "v1625591972/Alavastron%20posters/APOSTOLID_BALKAN_cpvxac.png"
 }, {
     "id" : 123,
-    "picture" : "v1621354287/logo1_n4on1d.png"
+    "picture" : "v1625591971/Alavastron%20posters/aLOVastrON_SCIFI_pkp2wd.png"
 }, {
     "id" : 124,
-    "picture" : "v1621354287/logo1_n4on1d.png"
+    "picture" : "v1625591968/Alavastron%20posters/alavastrondisco2018_fad6mx.png"
 }, {
     "id" : 125,
-    "picture" : "v1621354287/logo1_n4on1d.png"
+    "picture" : "v1625591965/Alavastron%20posters/2020_fixed_jpeg_fof6wk.jpg"
 }, {
     "id" : 126,
-    "picture" : "v1621354287/logo1_n4on1d.png"
+    "picture" : "v1625591965/Alavastron%20posters/balkan_ska_funk_september_2015_zfpuig.jpg"
 }, {
     "id" : 127,
-    "picture" : "v1621354287/logo1_n4on1d.png"
+    "picture" : "v1625591962/Alavastron%20posters/aposkitis_vos_xzjkqg.jpg"
 }, {
     "id" : 128,
-    "picture" : "v1621354287/logo1_n4on1d.png"
+    "picture" : "v1625591961/Alavastron%20posters/alavastron-friday_nights_hellofriends_z9xyta.jpg"
 }, {
     "id" : 129,
-    "picture" : "v1621354287/logo1_n4on1d.png"
+    "picture" : "v1625591959/Alavastron%20posters/alavastrinon_old_school_reunion_kiahvf.png"
 }, {
     "id" : 130,
-    "picture" : "v1621354287/logo1_n4on1d.png"
+    "picture" : "v1625591958/Alavastron%20posters/A3_summer_sunday_vibes_ev%C3%A6_ifsosw.png"
 }, {
     "id" : 131,
-    "picture" : "v1621354287/logo1_n4on1d.png"
+    "picture" : "v1625591955/Alavastron%20posters/aclockworkDJpartyVOL2_pvd9so.jpg"
 }, {
     "id" : 132,
-    "picture" : "v1621354287/logo1_n4on1d.png"
+    "picture" : "v1625591955/Alavastron%20posters/alavastron_nights_theory_of_harmony_idnyh3.jpg"
+}, {
+    "id" : 133,
+    "picture" : "v1625591954/Alavastron%20posters/46964702_559846917774278_171141750822273024_n_xxwjyl.png"
 }, {
     "id" : 134,
-    "picture" : "v1621354287/logo1_n4on1d.png"
+    "picture" : "v1625591951/Alavastron%20posters/23897265_1836839003242553_1189446502_o_evh4xt.jpg"
 }, {
     "id" : 135,
-    "picture" : "v1621354287/logo1_n4on1d.png"
+    "picture" : "v1625591951/Alavastron%20posters/28342387_219864578571584_45590472_o_fxsyev.jpg"
 }, {
     "id" : 136,
-    "picture" : "v1621354287/logo1_n4on1d.png"
+    "picture" : "v1625591950/Alavastron%20posters/82230368_781768275675191_3198288856285708288_n_jww2pi.jpg"
 }, {
     "id" : 137,
-    "picture" : "v1621354287/logo1_n4on1d.png"
+    "picture" : "v1625591949/Alavastron%20posters/17434614_973877429413437_616400908350702054_o_tzgcbk.jpg"
 }, {
     "id" : 138,
-    "picture" : "v1621354287/logo1_n4on1d.png"
+    "picture" : "v1625591949/Alavastron%20posters/12788321_10154711771773902_613515037_o_hcpx12.jpg"
 }, {
     "id" : 139,
-    "picture" : "v1621354287/logo1_n4on1d.png"
+    "picture" : "v1625591948/Alavastron%20posters/17039276_958510470950133_5494667540389998171_o_l0zpdl.jpg"
 }, {
     "id" : 140,
-    "picture" : "v1621354287/logo1_n4on1d.png"
+    "picture" : "v1625591948/Alavastron%20posters/12596815_10154602612478902_1984414329_o_kxmrgo.jpg"
 }, {
     "id" : 141,
-    "picture" : "v1621354287/logo1_n4on1d.png"
+    "picture" : "v1625591947/Alavastron%20posters/8years_w3mr7e.png"
 }, {
     "id" : 142,
-    "picture" : "v1621354287/logo1_n4on1d.png"
+    "picture" : "v1625591947/Alavastron%20posters/12218865_10154431930803902_1208893491_o_ubktoz.jpg"
 }, {
     "id" : 143,
-    "picture" : "v1621354287/logo1_n4on1d.png"
+    "picture" : "v1625591947/Alavastron%20posters/12736905_10154674095083902_1571442013_o_ps5izo.jpg"
 }, {
     "id" : 144,
-    "picture" : "v1621354287/logo1_n4on1d.png"
+    "picture" : "v1625591946/Alavastron%20posters/404_NOT_2_BE_FOUND_taojrq.png"
 }, {
     "id" : 145,
-    "picture" : "v1621354287/logo1_n4on1d.png"
+    "picture" : "v1625591944/Alavastron%20posters/1stoftheYear_hkd84w.png"
 }, {
     "id" : 146,
-    "picture" : "v1621354287/logo1_n4on1d.png"
+    "picture" : "v1625591945/Alavastron%20posters/11425547_644425285691988_1076989763526137730_o_tdm53o.jpg"
 }, {
     "id" : 147,
-    "picture" : "v1621354287/logo1_n4on1d.png"
+    "picture" : "v1625591945/Alavastron%20posters/10599210_1401244086802049_3430617520850588818_n_uu0xzo.jpg"
 }, {
     "id" : 148,
-    "picture" : "v1621354287/logo1_n4on1d.png"
+    "picture" : "v1625591943/Alavastron%20posters/10370363_544223622378822_126964777359247739_n_zxsw0q.jpg"
 }, {
     "id" : 149,
-    "picture" : "v1621354287/logo1_n4on1d.png"
+    "picture" : "v1625591943/Alavastron%20posters/1620560_417365968397922_1215073983_n_xvq5og.jpg"
 }, {
     "id" : 150,
-    "picture" : "v1621354287/logo1_n4on1d.png"
+    "picture" : "v1625591942/Alavastron%20posters/1528529_397108493757003_2038064945_n_zqyxqh.jpg"
 }, {
     "id" : 151,
-    "picture" : "v1621354287/logo1_n4on1d.png"
+    "picture" : "v1625591942/Alavastron%20posters/2017tktp_rdockt.jpg"
 }, {
     "id" : 152,
-    "picture" : "v1621354287/logo1_n4on1d.png"
+    "picture" : "v1625591940/Alavastron%20posters/1512379_397358473732005_1897322508_n_lyscxw.jpg"
 }, {
     "id" : 153,
-    "picture" : "v1621354287/logo1_n4on1d.png"
+    "picture" : "v1625591939/Alavastron%20posters/404_NOT_2_BE_FOUND_merged_NO_CIRCUIT_uhomsy.png"
 }, {
     "id" : 154,
-    "picture" : "v1621354287/logo1_n4on1d.png"
+    "picture" : "v1625591935/Alavastron%20posters/80s_electro_april_2018_j1zbf5.png"
 }, {
     "id" : 155,
-    "picture" : "v1621354287/logo1_n4on1d.png"
+    "picture" : "v1625591935/Alavastron%20posters/6th_alavastron_genethlion_sk5h6m.jpg"
 }, {
     "id" : 156,
-    "picture" : "v1621354287/logo1_n4on1d.png"
+    "picture" : "v1625591934/Alavastron%20posters/70s80s90s_2018_h8kxut.jpg"
 }, {
     "id" : 157,
-    "picture" : "v1621354287/logo1_n4on1d.png"
+    "picture" : "v1625591934/Alavastron%20posters/90sdecember2015_tnevr2.jpg"
 }, {
     "id" : 158,
-    "picture" : "v1621354287/logo1_n4on1d.png"
-}, {
-    "id" : 000,
-    "picture" : "v1621354287/logo1_n4on1d.png"
+    "picture" : "v1625591903/Alavastron%20posters/1SEEfire_rakwtb.png"
 }]
 
 posterData.forEach(function(obj) {
